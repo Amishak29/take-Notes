05684c80e6fe486bd496907637b22e1fdf247422
Amisha <amisha.kr29@gmail.com>
2025-01-21 23:23:20 +0530

    added

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const DbCon=async()=>{
+    try {
+        // mongoose.connect("mongodb://localhost:27017/YtNotesapp")
+        mongoose.connect(process.env.MONGODB_URL);
+        console.log('Mongodb is connected')
+        
+    } catch (error) {
+        console.log("Error in mongodb connection",error)
+    }
+}
+export default DbCon
