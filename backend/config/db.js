3d29043b9a0a10d34d533bb0afbb9e84fcac6ff8
Amisha <amisha.kr29@gmail.com>
2025-03-04 21:26:28 +0530

    added

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const DbCon=async()=>{
     try {
         // mongoose.connect("mongodb://localhost:27017/YtNotesapp")
-        mongoose.connect(process.env.MONGODB_URL);
+        mongoose.connect(process.env.MONGODB_URL).then(()=>{
+            console.log("connected")
+        });
         console.log('Mongodb is connected')
         
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
-import express from "express";
-import cookieParser from "cookie-parser";
+// import express from "express";
+// import cookieParser from "cookie-parser";
 
 
-import DbCon from "./db.js";
-import AuthRoutes from "./routes/Auth.js";
-import NotesRoutes from "./routes/Notes.js";
+// import DbCon from "./db.js";
+// import AuthRoutes from "./routes/Auth.js";
+// import NotesRoutes from "./routes/Notes.js";
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+// const app = express();
+// const PORT = process.env.PORT || 4000;
 
-// Middleware
-app.use(express.json());
-app.use(require("cookie-parser")());
-app.use(cookieParser());
+// // Middleware
+// app.use(express.json());
+// app.use(require("cookie-parser")());
+// app.use(cookieParser());
 
-// Database connection
-DbCon();
+// // Database connection
+// DbCon();
 
-// Routes
-app.use("/auth", AuthRoutes);
-app.use("/notes", NotesRoutes);
+// // Routes
+// app.use("/auth", AuthRoutes);
+// app.use("/notes", NotesRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
